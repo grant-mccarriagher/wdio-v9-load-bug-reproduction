0d4b62b2b086a5018d3d7caf6f5301f64b8c1399
grant-mccarriagher <gmccarriagher@acvauctions.com>
2024-12-12 11:10:43 -0500

    more

--- features/example.feature
+++ features/example.feature
@@ -1,5 +1,6 @@
 Feature: Example of mock call causing timeouts
 
-  Scenario: Mock call causing timeouts
+  Scenario: Page loads causing timeouts
     Given I have reached a page
+    When I load the same page again
     Then I should see an error
--- stepDefinitions/generalSteps.js
+++ stepDefinitions/generalSteps.js
@@ -1,17 +1,12 @@
-import { Given, Then } from '@cucumber/cucumber';
+import { Given, When, Then } from '@cucumber/cucumber';
 
 Given('I have reached a page', async function () {
-  const mock = await browser.mock('https://some-website.com/*');
-  mock.respond({}, {
-    statusCode: 404,
-    fetchResponse: false
-  });
-
   await browser.url('https://app.acvauctions.com/');
   await browser.pause(10000);
+});
 
+When('I load the same page again', async function () {
   await browser.url('https://app.acvauctions.com/');
-  await browser.pause(10000);
 });
 
 Then('I should see an error', async function () {
