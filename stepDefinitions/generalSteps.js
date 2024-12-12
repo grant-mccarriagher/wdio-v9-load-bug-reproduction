45c78457f161e2fe3dcd304732fe7a8fa6386785
grant-mccarriagher <gmccarriagher@acvauctions.com>
2024-12-12 11:23:06 -0500

    test

--- stepDefinitions/generalSteps.js
+++ stepDefinitions/generalSteps.js
@@ -2,11 +2,19 @@ import { Given, When, Then } from '@cucumber/cucumber';
 
 Given('I have reached a page', async function () {
   await browser.url('https://app.acvauctions.com/');
-  await browser.pause(10000);
+  await $('.acv-logo').waitForDisplayed();
+
+  const mock = await browser.mock('https://www.some-website.com/*');
+
+  mock.respond({}, {
+    statusCode: 404,
+    fetchResponse: false
+  });
 });
 
 When('I load the same page again', async function () {
   await browser.url('https://app.acvauctions.com/');
+  await $('.acv-logo').waitForDisplayed();
 });
 
 Then('I should see an error', async function () {
